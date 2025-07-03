c3c2999b08e1bd1a80547285ec38b663f94db43c
Felipe Gavilan <felipegavilan2202@gmail.com>
2025-07-03 12:14:47 -0500

    e

--- api/confirmacion.js
+++ api/confirmacion.js
@@ -1,11 +1,17 @@
 const admin = require('firebase-admin');
 
+console.log('ENV:', {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: typeof process.env.FIREBASE_PRIVATE_KEY,
+});
+
 // Inicializar Firebase solo una vez
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
