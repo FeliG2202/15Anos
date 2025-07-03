0c4b85ae3228eba4df75f6db16e94ad2e0f6e49e
Felipe Gavilan <felipegavilan2202@gmail.com>
2025-07-03 12:31:39 -0500

    h

--- api/confirmacion.js
+++ api/confirmacion.js
@@ -1,5 +1,7 @@
 const admin = require('firebase-admin');
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG);
+
 console.log('ENV:', {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -9,11 +11,7 @@ console.log('ENV:', {
 // Inicializar Firebase solo una vez
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 const db = admin.firestore();
--- api/confirmados.js
+++ api/confirmados.js
@@ -1,19 +1,11 @@
 const admin = require('firebase-admin');
 
-console.log('ENV:', {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: typeof process.env.FIREBASE_PRIVATE_KEY,
-});
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG);
 
 // Inicializar Firebase solo una vez
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 const db = admin.firestore();
