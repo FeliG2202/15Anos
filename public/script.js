4830f73a423b4c3c8fc4874fe99e69835cf4c605
Felipe Gavilan <felipegavilan2202@gmail.com>
2025-07-03 16:36:41 -0500

    update

--- public/script.js
+++ public/script.js
@@ -199,7 +199,7 @@ let audio = null
 
 function playMusic() {
   if (!audio) {
-    audio = new Audio("./music.mp3")
+    audio = new Audio("../public/music.mp3")
     audio.loop = true
     audio.volume = 0.3
   }
@@ -221,7 +221,7 @@ function toggleMusic() {
   const button = document.getElementById("musicToggle")
 
   if (!audio) {
-    audio = new Audio("./music.mp3")
+    audio = new Audio("../public/music.mp3")
     audio.loop = true
     audio.volume = 0.3
   }
